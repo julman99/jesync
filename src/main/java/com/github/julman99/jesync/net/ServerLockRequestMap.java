50aadb710083d730c7ea4d71050c3885c22ec847
Julio Viera <julio.viera@gmail.com>
2013-06-15 08:00:48 -0400

    Fixes #2

--- src/main/java/com/github/julman99/jesync/net/ServerLockRequestMap.java
+++ src/main/java/com/github/julman99/jesync/net/ServerLockRequestMap.java
@@ -1,12 +1,12 @@
 package com.github.julman99.jesync.net;
 
 import com.github.julman99.jesync.core.AbstractLockRequest;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Alias for HashMap<String, ServerLockRequest>
  * @author Julio Viera <julio.viera@gmail.com>
  */
-public class ServerLockRequestMap extends HashMap<String, AbstractLockRequest> {
+public class ServerLockRequestMap extends ConcurrentHashMap<String, AbstractLockRequest> {
     
 }
